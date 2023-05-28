877a60e997f2aac63f0bb8999deedec9d5bd9753
Zalaxis <130976751+z4laxis@users.noreply.github.com>
2023-05-28 17:45:58 +0100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,17 +1,17 @@
 -- Define constants
 local PLUGIN_NAME = "MyPlugin"
-local TOOLBAR_BUTTON_NAME = "OpenPluginButton"
+local TOOLBAR_BUTTON_NAME = "OpenPluginButton" -- Name of Plugin
 local PLUGIN_MENU_ITEM_NAME = "Open Plugin"
-local PLUGIN_ICON_ID = "rbxassetid://8073107221"
+local PLUGIN_ICON_ID = "rbxassetid://8073107221" -- Icon
 
 -- Define variables
-local toolbar = plugin:CreateToolbar("MyPluginToolbar")
+local toolbar = plugin:CreateToolbar("MyPluginToolbar") -- Bottom Text
 local toolbarButton = toolbar:CreateButton(TOOLBAR_BUTTON_NAME, "Open Plugin", PLUGIN_ICON_ID)
 local pluginMenu = plugin:CreatePluginMenu(PLUGIN_MENU_ITEM_NAME)
 
 -- Define UI variables
 local pluginWidget = plugin:CreateDockWidgetPluginGui(PLUGIN_NAME, DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Float, false, false, 300, 200, 250, 150))
-local mainFrame = Instance.new("Frame")
+local mainFrame = Instance.new("Frame") -- Interface
 mainFrame.Size = UDim2.new(1, 0, 1, 0)
 mainFrame.BackgroundColor3 = Color3.new(0.8, 0.8, 0.8)
 mainFrame.Parent = pluginWidget
